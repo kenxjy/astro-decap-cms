071ea977d476b0cabb044d36ca3ea4a1f64c04ae
Kenneth Yue <kennethyue39@gmail.com>
2023-05-07 14:20:47 -0400

    add decap cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import NetlifyCMS from 'astro-netlify-cms';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -6,5 +7,32 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		NetlifyCMS({
+			config: {
+				backend: {
+					name: 'github',
+					branch: 'master',
+					repo: 'kenxjy/astro-decap-cms'
+				},
+				collections: [
+					{
+						name: "blog",
+						label: "Blog",
+						folder: "src/content/blog",
+						create: true,
+						slug: "{{year}}-{{month}}-{{day}}-{{slug}}",
+						fields: [
+							{ label: "Title", name: "title", widget: "string" },
+							{ label: "Publish Date", name: "pubDate", widget: "datetime" },
+							{ label: "Updated Date", name: "updatedDate", widget: "datetime" },
+							{ label: "Hero Image", name: "heroImage", widget: "image" },
+						]
+					}
+				]
+			}
+		})
+	],
 });
