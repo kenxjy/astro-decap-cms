6dd685f6d919a6b5759876419a6c1d2d194d5697
Kenneth Yue <kennethyue39@gmail.com>
2023-05-07 21:12:55 -0400

    remove mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import NetlifyCMS from 'astro-netlify-cms';
-import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -8,7 +7,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [
-		mdx(),
 		sitemap(),
 		NetlifyCMS({
 			previewStyles: ['/src/styles/global.css'],
